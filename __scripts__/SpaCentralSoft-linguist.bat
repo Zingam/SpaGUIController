93d459e165487a13416a8a76799137ce94ed3c01
Zingam <zingam@outlook.com>
2014-09-17 15:45:18 +0300

    Added a couple of scripts to run the tools: lupdate, cloc

---
+++ __scripts__/SpaCentralSoft-linguist.bat
@@ -0,0 +1,12 @@
+REM Runs Qt SDK lupdate.exe tool
+REM Removes obsolete translation items from a project
+
+c:\SDKs\Qt\5.3\mingw482_32\bin\lupdate.exe -verbose -no-obsolete ..\GUIController\GUIController.pro
+
+
+
+
+
+
+
+
